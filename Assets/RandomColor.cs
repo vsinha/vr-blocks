19fd2e60fb9fab85452e7c5fb82f12f840674357
Viraj Sinha <virajosinha@gmail.com>
2017-04-13 14:58:34 -0700

    blocks spawn with random colors

---
+++ Assets/RandomColor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomColor : MonoBehaviour {
+
+    private static Color[] Colors =
+    {
+        Color.red,
+        Color.green,
+        Color.blue,
+        Color.white
+    };
+
+    public Color SelectRandomColor()
+    {
+        return Colors[Random.Range(0, Colors.Length)];
+    }
+}
