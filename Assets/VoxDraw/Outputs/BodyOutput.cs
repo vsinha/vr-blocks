8f93c2d7846b3584a4353bdedb426fb9d2c7a95a
Phil Price <pprice@microsoft.com>
2017-04-20 08:48:02 -0700

    Add better colliders to lines

--- Assets/VoxDraw/Outputs/BodyOutput.cs
+++ Assets/VoxDraw/Outputs/BodyOutput.cs
@@ -20,13 +20,50 @@ namespace VoxDraw.Outputs
             var lineRenderer = instance.GetComponent<VRLineRenderer>();
             lineRenderer.SetPositions(context.Positions.ToArray(), true);
 
-            var collider = instance.GetComponent<BoxCollider>();
-            collider.size = context.LocalBounds.size;
-            collider.center = context.LocalCenter;
-            collider.isTrigger = false;
-
+            // TODO: This is only for demos
+			// Create interpolated sphere coliders along the path of the line
 			var positions = context.Positions.ToArray (); 
+			Vector3 bbStart = positions [0];
+			Vector3 runningSum = bbStart;
+			int runningCount = 1;
+			int generated = 0;
 
+			const float threshold = 0.05f;
+
+			for (var i = 1; i < positions.Length; i++) { 
+				Vector3 bbCurrent = positions [i];
+				float distance = Vector3.Distance (bbStart, bbCurrent);
+
+				if (distance < threshold) {
+					runningCount++;
+					runningSum += bbCurrent;
+					continue;
+				}
+
+				var centroid = runningSum / runningCount; 
+				var angle = Vector3.Angle (bbStart, bbCurrent);
+
+				GameObject childBox = new GameObject ("Line Collision Point");
+				childBox.AddComponent<SphereCollider> ();
+				childBox.transform.SetParent (instance.transform);
+				childBox.transform.localScale = Vector3.one * 0.025f;
+				childBox.transform.localPosition = centroid; 
+				generated++;
+
+				runningSum = bbCurrent;
+				runningCount = 1; 
+				bbStart = bbCurrent;
+			}
+
+			if (generated == 0) { 
+				// Add just one 
+				GameObject childBox = new GameObject ();
+				childBox.AddComponent<SphereCollider> ();
+				childBox.transform.SetParent (instance.transform);
+				childBox.transform.localScale = Vector3.one * 0.025f;
+			}
+
+			// Create start and end snap points
 			if (context.NumberPoints >= 4) { 
 				// Take the end
 				var end = positions[positions.Length - 1];
@@ -39,12 +76,6 @@ namespace VoxDraw.Outputs
 				endSnapPoint.transform.localPosition = end;
 				endSnapPoint.transform.localScale = Vector3.one * 0.1f;
 				endSnapPoint.transform.rotation = Quaternion.LookRotation (direction) * Quaternion.Euler(0, 90, 0); 
-
-
-				var endCollider = endSnapPoint.GetComponent<SphereCollider> ();
-
-
-
 			}
 
 			if (context.NumberPoints >= 2) { 
@@ -60,10 +91,6 @@ namespace VoxDraw.Outputs
 				startSnapPoint.transform.localPosition = start;
 				startSnapPoint.transform.localScale = Vector3.one * 0.1f;
 				startSnapPoint.transform.rotation = Quaternion.LookRotation (direction) * Quaternion.Euler(0, 90, 0); 
-
-				var startCollider = startSnapPoint.GetComponent<SphereCollider> ();
-
-
 			}
         }
 
